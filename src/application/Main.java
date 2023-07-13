9eae333976e3433fcd1decd0e6352b6cf1501611
Áxel Vaz <106829115+Axelvazslima@users.noreply.github.com>
2023-07-12 21:54:15 -0300

    Moving Pieces Logic :art:

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,10 +1,24 @@
 package application;
 
 import chess.ChessMatch;
+import chess.ChessPiece;
+import chess.ChessPosition;
+
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         ChessMatch chessMatch = new ChessMatch();
-        UI.printBoard(chessMatch.getPieces());
+        Scanner sc = new Scanner(System.in);
+
+        while(true) {
+            UI.printBoard(chessMatch.getPieces());
+            System.out.print("\nSource: ");
+            ChessPosition source = UI.readChessPosition(sc);
+            System.out.print("\nTarget: ");
+            ChessPosition target = UI.readChessPosition(sc);
+
+            ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
+        }
     }
 }
