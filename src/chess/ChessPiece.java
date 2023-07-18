f89421745b7cf283871ce709dee67a742d47123b
Áxel Vaz <106829115+Axelvazslima@users.noreply.github.com>
2023-07-18 18:28:39 -0300

    Moves Counter :art:

--- src/chess/ChessPiece.java
+++ src/chess/ChessPiece.java
@@ -6,6 +6,7 @@ import boardgame.Position;
 
 public abstract class ChessPiece extends Piece {
     private Color color;
+    private int moveCount;
 
     public ChessPiece(Board board, Color color){
         super(board);
@@ -14,6 +15,18 @@ public abstract class ChessPiece extends Piece {
 
     public Color getColor(){
         return this.color;
+    }
+
+    public int getMoveCount(){
+        return this.moveCount;
+    }
+
+    public void increaseMoveCount(){
+        this.moveCount++;
+    }
+
+    public void decreaseMoveCount(){
+        this.moveCount--;
     }
 
     public ChessPosition getChessPosition(){
